3430384d7dfc2cf5b124026240bcadf5f42980ff
Evgeniy Kulikov <im@kulikov.im>
2018-04-24 23:47:54 +0300

    proof for @m1ome

--- package/config/config_test.go
+++ package/config/config_test.go
@@ -7,14 +7,29 @@ import (
 )
 
 func TestSetup(t *testing.T) {
-	opts := *DefaultOptions
-	opts.Path = "./fixtures"
+	t.Run("first", func(t *testing.T) {
+		opts := *DefaultOptions
+		opts.Path = "./fixtures/first"
 
-	conf, err := New(&opts)
+		conf, err := New(&opts)
 
-	assert.NoError(t, err)
-	assert.Equal(t, 1, conf.GetInt("a"))
-	assert.Equal(t, "string", conf.GetString("b"))
-	assert.Equal(t, 1, conf.GetInt("c.d"))
-	assert.Equal(t, "string", conf.GetString("c.e"))
+		assert.NoError(t, err)
+		assert.Equal(t, 1, conf.GetInt("a"))
+		assert.Equal(t, "string", conf.GetString("b"))
+		assert.Equal(t, 1, conf.GetInt("c.d"))
+		assert.Equal(t, "string", conf.GetString("c.e"))
+	})
+
+	t.Run("second", func(t *testing.T) {
+		opts := *DefaultOptions
+		opts.Path = "./fixtures/second"
+
+		conf, err := New(&opts)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 1, conf.GetInt("a"))
+		assert.Equal(t, "string", conf.GetString("b"))
+		assert.Equal(t, 1, conf.GetInt("c.d"))
+		assert.Equal(t, "string", conf.GetString("c.e"))
+	})
 }
